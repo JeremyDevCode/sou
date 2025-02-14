a34ee7a72ac8481c8c025ad58896142252ed85b2
JeremyDevCode <jeremydevcode@gmail.com>
2025-02-13 21:04:53 -0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,76 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [size, setSize] = useState(1);
+  const [noSize, setNoSize] = useState(1);
+  const [noPosition, setNoPosition] = useState({ x: 0, y: 0 });
+  const [accepted, setAccepted] = useState(false);
+  const [noCount, setNoCount] = useState(0);
+  const tooManyNo = noCount >= 10;
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+  const handleNoClick = () => {
+    setSize(size + 1);
+    setNoSize(noSize - 0.1);
+    setNoCount((prev) => prev + 1);
+    setNoPosition({
+      x: Math.random() * window.innerWidth * 0.5 - window.innerWidth * 0.25,
+      y: Math.random() * window.innerHeight * 0.5 - window.innerHeight * 0.25,
+    });
+  };
+
+  const getText = () => {
+    if (accepted) return "Te quiero muchito";
+    if (tooManyNo) return "Listo, te odio";
+    return "¿Quieres ser mi San Valentin?";
+  };
+
+  const getBackgroundColor = () => {
+    if (accepted) return "#DCCCE9";
+    if (tooManyNo) return "#FAFCF9";
+    return "#F9EFE9";
+  };
+
+  const getStatusImage = () => {
+    if (accepted)
+      return "https://media1.tenor.com/m/DDRz38sfMSIAAAAd/milk-mocha-bear.gif";
+    if (tooManyNo)
+      return "https://media1.tenor.com/m/umwFdDPwCJAAAAAd/i-have-been-bullied-a-lot-how-could-you-do-this-to-me.gif";
+    return "https://images-ext-1.discordapp.net/external/tKi9YcGW7wejplEquIWkMptg0zTwAW5eAmYCyGmWBTk/https/media.tenor.com/tAtFFcpDDgQAAAPo/milk-and-mocha-milk-mocha-bear.mp4";
+  };
+
+  console.log(tooManyNo);
+  return (
+    <div
+      className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
+      style={{ backgroundColor: getBackgroundColor() }}
+    >
+      <main className="flex flex-col gap-12 justify-center items-center font-bold text-3xl">
+        <h3 className="text-black">{getText()}</h3>
+        {!accepted && !tooManyNo && (
+          <div className="flex justify-between w-full">
+            <button
+              className="bg-green-500 px-8 py-4 rounded-lg"
+              style={{ transform: `scale(${size})` }}
+              onClick={() => setAccepted(true)}
+            >
+              Si
+            </button>
+            <button
+              className="bg-red-500 px-8 py-4 rounded-lg"
+              style={{
+                transform: `translate(${noPosition.x}px, ${noPosition.y}px) scale(${noSize})`,
+              }}
+              onClick={handleNoClick}
+            >
+              No
+            </button>
+          </div>
+        )}
+
+        <img src={getStatusImage()} />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
